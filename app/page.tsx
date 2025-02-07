3a5a0cd21bc4138573ac9ddc350c633fce5f73cb
bisratawoke <bisratawoke52@gmail.com>
2025-02-07 17:14:08 +0300

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import BottomDiscoverEthiopiaSection from "./components/BottomDiscoverEthiopia";
 import DiscoverEthiopiaSection from "./components/DiscorverEthiopia";
 import DiscorverEthiopiaSecondSection from "./components/DiscoverEthiopiaSectionSection";
 import FirstSection from "./components/FIrstSection";
-import LandingPageContainer from "./components/layout/landingPageContainer";
+import LandingPageContainer from "./components/layout/LandingPageContainer";
 import LinkCardsContainer from "./components/LinkCardsContainer";
 
 export default function Home() {
